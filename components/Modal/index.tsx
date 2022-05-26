4a8774830841fc2d6f0d292c0d4862ffc181b87e
abhikumar98 <abhishekkumar35962@gmail.com>
2022-05-27 00:39:09 +0530

    fix: modal css

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -31,7 +31,7 @@ const Modal = () => {
 							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 						>
-							<Dialog.Panel className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+							<Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8">
 								<div>
 									<div className="mt-3 text-center sm:mt-5">
 										<Dialog.Title
@@ -41,109 +41,107 @@ const Modal = () => {
 											Payment successful
 										</Dialog.Title>
 										<div className="mt-2">
-											<div className="h-[600px] bg-gray-100">
-												<div className="flex items-center justify-center w-full h-full px-4 py-5 sm:p-6">
-													<div className="w-full max-w-sm bg-white shadow-lg rounded-xl">
-														<div className="px-4 py-5 sm:p-6">
-															<p className="text-xl font-bold text-gray-900">
-																Add New User
-															</p>
-															<p className="mt-3 text-sm font-medium text-gray-500">
-																Lorem ipsum
-																dolor sit amet,
-																consec tetur
-																adipiscing elit.
-															</p>
+											<div className="flex items-center justify-center w-full h-full">
+												<div className="w-full max-w-sm bg-white shadow-lg rounded-xl">
+													<div className="px-4 py-5 sm:p-6">
+														<p className="text-xl font-bold text-gray-900">
+															Add New User
+														</p>
+														<p className="mt-3 text-sm font-medium text-gray-500">
+															Lorem ipsum dolor
+															sit amet, consec
+															tetur adipiscing
+															elit.
+														</p>
 
-															<form
-																action="#"
-																method="POST"
-																className="mt-6"
-															>
-																<div className="space-y-4">
-																	<div>
-																		<label
-																			for=""
-																			className="text-sm font-bold text-gray-900"
-																		>
-																			{" "}
-																			Name{" "}
-																		</label>
-																		<div className="mt-2">
-																			<input
-																				type="text"
-																				name=""
-																				id=""
-																				placeholder="John Doe"
-																				value=""
-																				className="block w-full px-4 py-3 placeholder-gray-500 border -gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600"
-																			/>
-																		</div>
-																	</div>
-
-																	<div>
-																		<label
-																			for=""
-																			className="text-sm font-bold text-gray-900"
-																		>
-																			{" "}
-																			Email{" "}
-																		</label>
-																		<div className="mt-2">
-																			<input
-																				type="text"
-																				name=""
-																				id=""
-																				placeholder="email@example.com"
-																				value=""
-																				className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600"
-																			/>
-																		</div>
-																	</div>
-
-																	<div>
-																		<label
-																			for=""
-																			className="text-sm font-bold text-gray-900"
-																		>
-																			{" "}
-																			User
-																			Role{" "}
-																		</label>
-																		<div className="mt-2">
-																			<select className="block w-full py-3 pl-4 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm">
-																				<option>
-																					Editor
-																				</option>
-																			</select>
-																		</div>
+														<form
+															action="#"
+															method="POST"
+															className="mt-6"
+														>
+															<div className="space-y-4">
+																<div>
+																	<label
+																		for=""
+																		className="text-sm font-bold text-gray-900"
+																	>
+																		{" "}
+																		Name{" "}
+																	</label>
+																	<div className="mt-2">
+																		<input
+																			type="text"
+																			name=""
+																			id=""
+																			placeholder="John Doe"
+																			value=""
+																			className="block w-full px-4 py-3 placeholder-gray-500 border -gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600"
+																		/>
 																	</div>
 																</div>
 
-																<div className="flex items-center justify-end mt-5 space-x-4">
-																	<button
-																		type="reset"
-																		className="inline-flex items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-gray-600 transition-all duration-200 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:bg-gray-50 hover:text-gray-900"
+																<div>
+																	<label
+																		for=""
+																		className="text-sm font-bold text-gray-900"
 																	>
-																		Cancel
-																	</button>
-
-																	<button
-																		type="submit"
-																		className="inline-flex items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-white transition-all duration-200 bg-indigo-600 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 hover:bg-indigo-500"
-																	>
-																		Add
-																	</button>
+																		{" "}
+																		Email{" "}
+																	</label>
+																	<div className="mt-2">
+																		<input
+																			type="text"
+																			name=""
+																			id=""
+																			placeholder="email@example.com"
+																			value=""
+																			className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-lg focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm caret-indigo-600"
+																		/>
+																	</div>
 																</div>
-															</form>
-														</div>
+
+																<div>
+																	<label
+																		for=""
+																		className="text-sm font-bold text-gray-900"
+																	>
+																		{" "}
+																		User
+																		Role{" "}
+																	</label>
+																	<div className="mt-2">
+																		<select className="block w-full py-3 pl-4 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm">
+																			<option>
+																				Editor
+																			</option>
+																		</select>
+																	</div>
+																</div>
+															</div>
+
+															<div className="flex items-center justify-end mt-5 space-x-4">
+																<button
+																	type="reset"
+																	className="inline-flex items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-gray-600 transition-all duration-200 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:bg-gray-50 hover:text-gray-900"
+																>
+																	Cancel
+																</button>
+
+																<button
+																	type="submit"
+																	className="inline-flex items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-white transition-all duration-200 bg-indigo-600 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 hover:bg-indigo-500"
+																>
+																	Add
+																</button>
+															</div>
+														</form>
 													</div>
 												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-								<div className="mt-5 sm:mt-6">
+								{/* <div className="mt-5 sm:mt-6">
 									<button
 										type="button"
 										className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
@@ -151,7 +149,7 @@ const Modal = () => {
 									>
 										Go back to dashboard
 									</button>
-								</div>
+								</div> */}
 							</Dialog.Panel>
 						</Transition.Child>
 					</div>
