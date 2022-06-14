bea80ff1ea6ab181ebe7358d45e80c80805c2fe4
github-Dheeraj <65020178+github-Dheeraj@users.noreply.github.com>
2022-06-15 01:15:42 +0530

    update index.tsx with smart contract func calls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
+import { stringify } from "querystring";
 import { useState } from "react";
 import { useAccount, useBalance } from "wagmi";
 
 import BetCard from "../components/BetCard";
 import CreateBet from "../components/Modal/CreateBet";
 import { contract } from "../utils/crypto";
-
+import { ethers } from "ethers";
+import { formatEther, parseEther } from "ethers/lib/utils";
 const Home: NextPage = () => {
 	const [showWalletOptions, setShowWalletOptions] = useState(false);
 	const [{ data: accountData, loading: accountLoading }] = useAccount();
@@ -23,6 +25,72 @@ const Home: NextPage = () => {
 		console.log(res);
 	};
 
+	const updateContractOwners = async(addOwnerAddress: string) =>{
+		var name:any = accountData?.address; 
+		const updateOwnerTx =  await contract().updateOwners( addOwnerAddress, {from: name} );
+		await updateOwnerTx.wait();
+		return updateOwnerTx;
+	}
+
+	const removeContractOwners = async(removeOwnerAddress: string) =>{
+		var name:any = accountData?.address; 
+		const removeOwnerTx =  await contract().removeOwner( removeOwnerAddress, {from: name} );
+		await removeOwnerTx.wait();
+		return removeOwnerTx;
+	}
+
+	const createMatchs = async (teamOne: string, teamTwo: string)=> {
+		var name:any = accountData?.address; 
+		const matchTransaction = await await contract()._createMatchs(teamOne, teamTwo, {from: name});
+		await matchTransaction.wait()
+		return matchTransaction;
+	}
+
+	const createBet = async (matchId: any,teamName: string, maticAmount: string)=> {
+		const createBetTransaction = await contract()._createBet(matchId, teamName, { value: ethers.utils.parseEther(maticAmount) });
+		await createBetTransaction.wait();
+		return createBetTransaction;
+	}
+	const joinBet = async (matchId: any)=> {
+		const joinBetTransaction = await contract()._joinBet(matchId);
+		await joinBetTransaction.wait();
+		return joinBetTransaction;
+	} 
+	const setClaimableAmount = async (claimAddress: string,maticAmount: string)=> { 
+		var name:any = accountData?.address; 
+		const claimAmtTransaction =  await contract().setbetWinnerAmountClaimable(claimAddress,{ value: ethers.utils.parseEther(maticAmount) }, {from: name} );
+		await claimAmtTransaction.wait();
+		return claimAmtTransaction;
+	}
+	const updateWinningTeam = async (matchId: any, winTeamName: string) =>{
+		var name:any = accountData?.address; 
+		const updateWinningTeamFuncTx =  await contract().updateWinningTeam(matchId,winTeamName, {from:name});
+		await updateWinningTeamFuncTx.wait();
+		return updateWinningTeamFuncTx;
+	}
+
+	const claimWinning = async ()=>{
+		var name:any = accountData?.address; 
+		const claimWinningFuncTx =  await contract().connect(name).claimYourWinning();
+		await claimWinningFuncTx.wait();
+		return claimWinningFuncTx;
+	}
+
+	const withdrawContractTotalFunds = async()=>{
+		var name:any = accountData?.address; 
+		const withdrawFundTx =  await contract().withdrawFunds({from: name});
+		await withdrawFundTx.wait();
+		return withdrawFundTx;
+		
+	}
+
+	const toggleContractStatus = async()=>{
+		var name:any = accountData?.address; 
+		const toggleContracttx =  await contract().connect(name).toggleStatus();
+		await toggleContracttx.wait();
+		return toggleContracttx;
+		
+	}
 	return (
 		<>
 			{/* <WalletOptionsModal
